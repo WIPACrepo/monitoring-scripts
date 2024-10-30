50721aaaeab0721cd5969ef333b3c5673f7fc321
Alec <asheperd@icecube.wisc.edu>
2024-10-30 11:33:08 -0500

    update condor to es scripts to es version 8.15 & python 3.11

--- condor_history_to_es.py
+++ condor_history_to_es.py
@@ -6,23 +6,29 @@ Read from condor history and write to elasticsearch
 
 import os
 import glob
-from optparse import OptionParser
+from argparse import ArgumentParser
 import logging
 from functools import partial
+from rest_tools.client import ClientCredentialsAuth
 
-parser = OptionParser('usage: %prog [options] history_files')
-parser.add_option('-a','--address',help='elasticsearch address')
-parser.add_option('-n','--indexname',default='condor',
+parser = ArgumentParser('usage: %prog [options] history_files')
+parser.add_argument('-a','--address',help='elasticsearch address')
+parser.add_argument('-n','--indexname',default='condor',
                   help='index name (default condor)')
-parser.add_option('--dailyindex', default=False, action='store_true',
+parser.add_argument('--dailyindex', default=False, action='store_true',
                   help='Index pattern daily')
-parser.add_option("-y", "--dry-run", default=False,
+parser.add_argument("-y", "--dry-run", default=False,
                   action="store_true",
                   help="query jobs, but do not ingest into ES",)
-parser.add_option('--collectors', default=False, action='store_true',
+parser.add_argument('--collectors', default=False, action='store_true',
                   help='Args are collector addresses, not files')
-(options, args) = parser.parse_args()
-if not args:
+parser.add_argument('--client_id',help='oauth2 client id')
+parser.add_argument('--client_secret',help='oauth2 client secret')
+parser.add_argument('--token_url',help='oauth2 realm token url')
+parser.add_argument("positionals", nargs='+')
+
+options = parser.parse_args()
+if not options.positionals:
     parser.error('no condor history files or collectors')
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(name)s : %(message)s')
@@ -36,7 +42,6 @@ def es_generator(entries):
         data['_index'] = options.indexname
         if options.dailyindex:
             data['_index'] += '-'+(data['date'].split('T')[0].replace('-','.'))
-        data['_type'] = 'job_ad'
         data['_id'] = data['GlobalJobId'].replace('#','-').replace('.','-')
         if not data['_id']:
             continue
@@ -50,10 +55,21 @@ address = options.address
 if '://' in address:
     prefix,address = address.split('://')
 
+api = ClientCredentialsAuth(address='https://elasticsearch.icecube.aq',
+                            token_url=options.token_url,
+                            client_secret=options.client_secret,
+                            client_id=options.client_id)
+token = api.make_access_token()
+
+print(token)
+
 url = '{}://{}'.format(prefix, address)
 logging.info('connecting to ES at %s',url)
 es = Elasticsearch(hosts=[url],
-                   timeout=5000)
+                   request_timeout=5000,
+                   bearer_auth=token)
+
+print('made it here')
 
 def es_import(document_generator):
     if options.dry_run:
@@ -63,12 +79,12 @@ def es_import(document_generator):
             json.dump(hit, sys.stdout)
         success = True
     else:
-        success, _ = bulk(es, document_generator, max_retries=20, initial_backoff=2, max_backoff=3600)
+        success, _ = bulk(es, document_generator, max_retries=20, initial_backoff=2, max_backoff=360)
     return success
 
 failed = False
 if options.collectors:
-    for coll_address in args:
+    for coll_address in options.positionals:
         try:
             gen = es_generator(read_from_collector(coll_address, history=True))
             success = es_import(gen)
@@ -76,8 +92,10 @@ if options.collectors:
             failed = e
             logging.error('Condor error', exc_info=True)
 else:
-    for path in args:
-        for filename in glob.iglob(path):
+    print(options.positionals)
+    for path in options.positionals:
+        print(path)
+        for filename in glob.glob(path):
             gen = es_generator(read_from_file(filename))
             success = es_import(gen)
             logging.info('finished processing %s', filename)
